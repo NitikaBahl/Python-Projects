213d862de6426c61e98067968b2190941194a8a0
“NitikaBahl” <nitikabahl21@gmail.com>
2024-12-19 22:37:20 +0530

    Recursion

---
+++ recursion.py
@@ -0,0 +1,22 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+def fibonacci(n):
+    if n <=1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+def sum_of_list(lst):
+    if not lst:
+        return 0
+    else:
+        return lst[0] + sum_of_list(lst[1:])
+    
+if __name__=="__main__":
+    print(factorial(5))
+    print(fibonacci(10))
+    print(sum_of_list([1,2,3,4,5]))
